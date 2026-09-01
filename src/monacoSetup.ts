69eef9cde66fd1f8c6fe085b9624279fc9f10c94
Venkat Sai Kumar Kandavalli <166972510+Thevenky@users.noreply.github.com>
2026-09-01 14:19:52 +0530

    Add core app entry files and beginner question bank

---
+++ src/monacoSetup.ts
@@ -0,0 +1,11 @@
+import { loader } from "@monaco-editor/react";
+import * as monaco from "monaco-editor";
+import editorWorker from "monaco-editor/editor/editor.worker.js?worker";
+
+self.MonacoEnvironment = {
+  getWorker() {
+    return new editorWorker();
+  },
+};
+
+loader.config({ monaco });
